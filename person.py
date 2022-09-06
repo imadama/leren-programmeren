1012338a16c488d9887c0f6839d25972c81e3c09
imadama <99074774@mydavinci.nl>
2022-09-06 13:44:45 +0200

    person

---
+++ person.py
@@ -0,0 +1,24 @@
+print('naaminvoer')
+naam = input ('geef jouw naam')
+print('hallo ' + naam)
+
+print('naaminvoer')
+adres = input ('geef jouw adres')
+print(adres)
+
+print('naaminvoer')
+postcode = input ('geef jouw postcode')
+print(postcode)
+
+print('naaminvoer')
+woonplaats = input ('geef jouw woonplaats')
+print(woonplaats)
+
+
+
+print(' ---------------------------------------------------')
+print('|Naam         :' + naam)
+print('|adres        :' + adres)
+print('|postcode     :' + postcode)
+print('|woonplaats   :' + woonplaats)
+print(' ---------------------------------------------------')
--- x = 1239,00.py
+++ x = 1239,00.py
@@ -1,4 +1,24 @@
-x = 1239,00
-y = 18
-result  = x - y
-print (result)
+print('naaminvoer')
+naam = input ('geef jouw naam')
+print('hallo ' + naam)
+
+print('naaminvoer')
+adres = input ('geef jouw adres')
+print(adres)
+
+print('naaminvoer')
+postcode = input ('geef jouw postcode')
+print(postcode)
+
+print('naaminvoer')
+woonplaats = input ('geef jouw woonplaats')
+print(woonplaats)
+
+
+
+print('----------------------------------------------------')
+print('Naam         :' + naam)
+print('adres        :' + adres)
+print('postcode     :' + postcode)
+print('woonplaats   :' + woonplaats)
+print('----------------------------------------------------')
