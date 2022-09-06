9b6b6b282619a86d0569509edf32dc558294b27e
imadama <99074774@mydavinci.nl>
2022-09-06 11:57:17 +0200

    feestlunch speelhal

--- feestlunch.py
+++ feestlunch.py
@@ -1,6 +1,6 @@
 from http.client import OK
 
-
+print ('De feestlunch kost je bij de bakker 10,69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
 croissant = 0.39
 stokbrood = 2.78
 kortingsbonnen= 0.50
--- speelhal.py
+++ speelhal.py
@@ -1,4 +1,4 @@
-print ("Dagje uit met 3 vrienden en jezelf kost")
+print ("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 43,12 euro")
 ticket = 7.45
 vrticket = 0.37 #per 5 min
 print ((ticket*4)+ (((45/5) * vrticket) * 4))
