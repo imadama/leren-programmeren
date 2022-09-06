1c8f5901a4746659f60f6f78afe74a4eb1128af5
imadama <99074774@mydavinci.nl>
2022-09-06 13:06:37 +0200

    person speelhal

--- speelhal.py
+++ speelhal.py
@@ -1,5 +1,4 @@
-print ("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 43,12 euro")
 ticket = 7.45
 vrticket = 0.37 #per 5 min
-print ((ticket*4)+ (((45/5) * vrticket) * 4))
-
+kosten=  ((ticket*4)+ (((45/5) * vrticket) * 4))
+print ("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {} euro". format (kosten))
