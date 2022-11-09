6530c09638397431ec12a3c878224e2fd8b9f8e6
imadama <99074774@mydavinci.nl>
2022-11-09 16:46:43 +0100

    idk

---
+++ Module 3/detijd.py
@@ -0,0 +1,4 @@
+for i in range(1,13):
+    print(i, "AM")
+for i in range(1,13):
+    print(i, "PM")
--- Module 3/tafelmanieren.py
+++ Module 3/tafelmanieren.py
@@ -1,6 +1,4 @@
-# tafelmanieren
+nummer = int(input("Welke tafel wil jij weten?"))
 
-getal = int(input("Welke tafel wilt u brekenen? "))
-
-for i in range (1,11): 
-    print(f"{i} x {getal} = {i * getal}")
+for i in range(1,11):
+    print(f"{i} *  {nummer} = {i * nummer}")
