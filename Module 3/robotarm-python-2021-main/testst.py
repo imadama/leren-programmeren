635e743fc1acfe275ab2566f0639d717b3d28cf4
imadama <99074774@mydavinci.nl>
2022-11-10 09:41:41 +0100

    renew

--- Module 3/robotarm-python-2021-main/testst.py
+++ Module 3/robotarm-python-2021-main/testst.py
@@ -1,25 +1,9 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 4')
-robotArm.speed = 3
+robotArm = RobotArm('exercise 5')
+
 # Jouw python instructies zet je vanaf hier:
+for x in range(8)
 
-for i in range(0,4):
-    robotArm.grab()
-    robotArm.moveRight()
-    robotArm.moveRight()
-    robotArm.drop()
-    robotArm.moveLeft()
-    robotArm.moveLeft()
-    
-robotArm.grab()
-robotArm.moveRight()
-robotArm.drop()
-
-for i in range(1,5):
-    robotArm.moveRight()
-    robotArm.grab()
-    robotArm.moveLeft()
-    robotArm.drop()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
