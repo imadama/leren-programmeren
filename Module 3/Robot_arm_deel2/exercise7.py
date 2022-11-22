b2cb1225d9474b8052e2bb1e3be1d4eb85baf0dc
imadama <99074774@mydavinci.nl>
2022-11-22 13:13:42 +0100

    ex7

--- Module 3/Robot_arm_deel2/exercise7.py
+++ Module 3/Robot_arm_deel2/exercise7.py
@@ -1,9 +1,14 @@
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 7')
-
+robotArm.speed = 4
 # Jouw python instructies zet je vanaf hier:
-
-
+for x in range(0,6):
+    robotArm.moveRight()
+    for y in range(0,6):
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+        robotArm.moveRight()
+    robotArm.moveRight()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
