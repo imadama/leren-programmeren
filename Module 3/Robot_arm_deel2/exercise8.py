8ff1591b7b78f9c086047c82e2456567dc57f840
imadama <99074774@mydavinci.nl>
2022-11-22 13:38:06 +0100

    r

--- Module 3/Robot_arm_deel2/exercise8.py
+++ Module 3/Robot_arm_deel2/exercise8.py
@@ -1,9 +1,12 @@
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 8')
-
 # Jouw python instructies zet je vanaf hier:
-
-
+for x in range(8):
+    robotArm.moveRight()
+    for y in range(0,7):
+        robotArm.grab()
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveLeft()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
